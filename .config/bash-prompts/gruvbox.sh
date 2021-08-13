4fca774ce9aaa1fc16f330397415cdbfcfc7056b
Prashantha T P <tpprashantha@gmail.com>
2021-08-13 22:56:29 +0530

    (feat) place cursor in nextline
    config copied from minimal.sh

--- .config/bash-prompts/gruvbox.sh
+++ .config/bash-prompts/gruvbox.sh
@@ -123,6 +123,8 @@ function __prompt {
     PS1+="\001${BLACK2_BG}\002\001${GRAY}\002 \w \001${RESET}\002"                # ~
     PS1+="\$(prompt_git)"                                                          # <git branch>
     PS1+="\$(status_prefix)"                                                       # $
+	PS1+="\n\001${GRAY}└─${RESET}\002${GRAY} \$${RESET}${GRAY} ▶${RESET} "
+
   }
 
   PROMPT_COMMAND=init_prompt
