4ec3cbe9431ea55d19630ea23b3a756442c631fe
Prashantha T P <tpprashantha@gmail.com>
2021-09-01 17:19:35 +0530

    (feat) change bash prompt style
    add color variables and git branch functions

--- .config/bash-prompts/minimal.sh
+++ .config/bash-prompts/minimal.sh
@@ -1,46 +1,54 @@
-    BOLD=$(tput bold)
-    RESET=$(tput sgr0)
-    if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
-      BLACK=$(tput setaf 235)
-      BLACK0=$(tput setaf 0)
-      BLACK0_BG=$(tput setab 0)
-      BLACK1=$(tput setaf 237)
-      BLACK1_BG=$(tput setab 237)
-      BLACK2=$(tput setaf 239)
-      BLACK2_BG=$(tput setab 239)
-      RED=$(tput setaf 124)
-      RED_BG=$(tput setab 124)
-      GRAY=$(tput setaf 246)
-      GRAY_BG=$(tput setab 246)
-      WHITE=$(tput setaf 223)
-      ORANGE_BG=$(tput setab 208)
-      YELLOW=$(tput setaf 178 )
-      YELLOW_BG=$(tput setab 178 )
-      GRAY_2=$(tput setaf 252)
-      GRAY_2_BG=$(tput setab 252)
-    else
-      BLACK=$(tput setaf 0)
-      BLACK1=$(tput setaf 0)
-      BLACK1_BG=$(tput setab 0)
-      BLACK2=$(tput setaf 0)
-      BLACK2_BG=$(tput setab 0)
-      RED=$(tput setaf 1)
-      RED_BG=$(tput setab 1)
-      GRAY=$(tput setaf 7)
-      GRAY_BG=$(tput setab 7)
-      WHITE=$(tput setaf 7)
-      ORANGE_BG=$(tput setab 3)
-      YELLOW_BG=GRAY
-    fi
-
-PS1='\[\033[0;32m\]\[\033[0m\033[0;32m\]\u\[\033[0;36m\] @ \[\033[0;36m\]\h \w\[\033[0;32m\]$(__git_ps1)'
-
-if [[ ${#VIRTUAL_ENV} -gt 0 ]]
-then
-	PS1+=$' \uE235 '
-	PS1+="$(basename ${VIRTUAL_ENV}) "
+#! /bin/bash
+colorize(){
+	local temp=$1
+	echo "\001${!temp}\002"
+}
+gitbranch_helper() {
+        echo -n '(' && git branch 2>/dev/null | grep '^*' | colrm 1 2 | tr -d '\n' && echo  -n ')'
+}
+git_branch() {
+        gitbranch_helper | sed 's/()//'
+}
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
+  BLACK0=$(tput setaf 0)
+  BLACK0_BG=$(tput setab 0)
+  RED=$(tput setaf 9)
+  RED_BG=$(tput setab 9)
+  GRAY=$(tput setaf 8)
+  GRAY_BG=$(tput setab 8)
+  GREEN=$(tput setaf 113)
+  GREEN_BG=$(tput setab 113)
+  WHITE=$(tput setaf 15)
+  WHITE_BG=$(tput setab 15)
+  ORANGE=$(tput setaf 208)
+  ORANGE_BG=$(tput setab 208)
+  GOLD=$(tput setaf 178)
+  GOLD_BG=$(tput setab 178)
+  GOLD3=$(tput setaf 142)
+  GOLD3_BG=$(tput setab 142)
+  YELLOW=$(tput setaf 178 )
+  YELLOW_BG=$(tput setab 178 )
+  Chartreuse3=$(tput setaf 76)
+  Chartreuse3_BG=$(tput setab 76)
 fi
 
-PS1+='\n\[\033[0;32m\]└─\[\033[0m\033[0;32m\] \$\[\033[0m\033[0;32m\] ▶\[\033[0m\] '
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWCOLORHINTS=1
+export GIT_PS1_SHOWUNTRACKEDFILES=
+PS1="\n$(colorize GOLD)\u$(colorize RESET)$(colorize GREEN) @ $(colorize GOLD3)\h :: \w$(colorize GOLD) \$(__git_ps1) $(colorize RESET)"
+
+
+# if [[ ${#VIRTUAL_ENV} -gt 0 ]]
+# then
+	# PS1+=$' \uE235 '
+	# PS1+="$(basename ${VIRTUAL_ENV}) "
+# fi
+
+PS1+="\n$(colorize GREEN)└─\$ ▶ $(colorize RESET) "
 
 export PS1
